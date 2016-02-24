e24f100a460bd24945cc6bc9cc12d8c80464f129
sasaki <administrator@ultra-unko.com>
2016-02-25 08:52:50 +0900

    ﾁｪｯｸﾌｧｲﾙ更新

--- TonNurakoEx/scripts.app/Contents/Resources/LibCheck.c
+++ TonNurakoEx/scripts.app/Contents/Resources/LibCheck.c
@@ -1,8 +1,48 @@
 #include <Xm/Xm.h>
 #include <X11/xpm.h>
-#include <stdio.h>
-int main()
+#include <X11/Xlib.h>
+#include<X11/Intrinsic.h>
+
+int
+XtTest(int argc, char** argv)
 {
+    Widget toplevel;
+    Display* display;
+    XtAppContext context;
+
+
+    context = XtCreateApplicationContext();
+
+    display = XtOpenDisplay(
+        context, NULL, NULL, NULL, NULL, 0, &argc, argv);
+    if (NULL == display) {
+        return 0;
+    }
+
+    toplevel = XtAppCreateShell("k",
+                    "k", applicationShellWidgetClass, display,NULL, 0);
+    if (NULL == toplevel) {
+        return -2;
+    }
+    XtRealizeWidget(toplevel);
+    if (argc > 1) {
+        XtAppMainLoop(context);
+    }
+    XtDestroyWidget(toplevel);
+    return 0;
+}
+
+int
+main(int argc, char** argv)
+{
+    int retVal;
+    XtToolkitInitialize();
+
+
+    if (0 != (retVal = XtTest(argc, argv))) {
+        fprintf(stderr, "LibCheck: XtTest FAILED(%d)\n", retVal);
+        return retVal;
+    }
     fprintf(stderr, "LibCheck OK\n");
     return 0;
 }
