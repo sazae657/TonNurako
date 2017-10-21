8865f70913fe55749a7890dac7dcb859754d53d0
sasaki <info@ftp.gawanative.com>
2017-10-21 17:21:33 +0900

    ﾞﾌﾛﾞｾｸﾄ追加

---
+++ demo/Xt/XtMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TonNurako;
+using TonNurako.Xt;
+
+namespace Frogram {
+    class Program : TonNurako.Widgets.Shell.TopLevel {
+
+        public override void ShellCreated() {
+            base.ShellCreated();
+            var t = Translations.ParseTranslationTable("<Message>WM_PROTOCOLS: quit()");
+            TonNurako.Inutility.Dumper.DumpProperty(t, (s)=> Console.WriteLine($"{s}"));
+        }
+
+
+        static void Main(string[] args) {
+            System.Diagnostics.Debug.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(Console.Out));
+
+            var app = new TonNurako.ApplicationContext();
+            var prog = new Program();
+            prog.Name = "name";
+            TonNurako.Application.Run(app, prog);
+        }
+    }
+}
